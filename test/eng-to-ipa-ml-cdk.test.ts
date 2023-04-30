ba00b0992236670456d07be7f0eecb7733ee9ac2
order&chaos <sean@orderandchaoscreative.com>
2023-04-30 12:29:14 +0100

    Initial commit

---
+++ test/eng-to-ipa-ml-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EngToIpaMlCdk from '../lib/eng-to-ipa-ml-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eng-to-ipa-ml-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EngToIpaMlCdk.EngToIpaMlCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
